f63b28dea384fb3a93e488c8cb0b460d18a079c9
FaizNation <novaatalagrab@gmail.com>
2026-01-30 21:48:38 +0700

    feat: initial mason

---
+++ bricks/clean_feature/__brick__/{{name.snakeCase()}}/data/repositories/{{name.snakeCase()}}_repository_impl.dart
@@ -0,0 +1,5 @@
+import '../../domain/repositories/{{name.snakeCase()}}_repository.dart';
+
+class {{name.pascalCase()}}RepositoryImpl implements {{name.pascalCase()}}Repository {
+  // Implement methods here
+}
